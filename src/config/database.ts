805412891c919b756c0d88af7d1c8af3991998f1
alex texis <alextexis.js@gmail.com>
2021-03-01 11:31:27 -0600

    add db prod environment

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,15 @@
-import { DB_NAME, DB_USER, DB_HOST, DB_PASSWORD } from '../shared/env';
+import {
+  DB_NAME,
+  DB_PROD_NAME,
+  DB_USER,
+  DB_HOST,
+  DB_PASSWORD,
+} from '../shared/env';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const settings = {
-  database: DB_NAME,
+  database: isProd ? DB_PROD_NAME : DB_NAME,
   host: DB_HOST,
   username: DB_USER,
   password: DB_PASSWORD,
