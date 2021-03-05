65929057f9dbbca49790e283907a42ee7bd6a780
alex texis <alextexis.js@gmail.com>
2021-03-05 16:42:44 -0600

    update: get query param limit_items of find episodes controller

--- src/controllers/episode/findEpisodes.ts
+++ src/controllers/episode/findEpisodes.ts
@@ -3,6 +3,7 @@ import { Episode } from '../../models';
 
 interface EpisodeQuerys {
   sort_createdAt?: 'asc' | 'desc';
+  limit_items?: string;
 }
 
 export const findEpisodes = async (
@@ -10,13 +11,17 @@ export const findEpisodes = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { sort_createdAt }: EpisodeQuerys = req.query;
+  const { sort_createdAt, limit_items }: EpisodeQuerys = req.query;
   const sort: Record<string, any> = {};
+  const limit = parseInt(limit_items || '12');
 
   if (sort_createdAt) sort.createdAt = sort_createdAt;
 
   try {
-    const episodes = await Episode.find().sort(sort).populate('serie');
+    const episodes = await Episode.find()
+      .sort(sort)
+      .limit(limit)
+      .populate('serie');
 
     res.status(200).json(episodes);
   } catch (e) {
