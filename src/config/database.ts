802fd7aa1c3b72d0e827e87cd6efa3cbd3ba6097
alexTexis <alextexis.js@gmail.com>
2021-03-25 23:28:04 -0600

    update: environment variables, replace dotenv with dotenv-flow, vercel json

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,15 +1,7 @@
-import {
-  DB_NAME,
-  DB_PROD_NAME,
-  DB_USER,
-  DB_HOST,
-  DB_PASSWORD,
-} from '../utils/env';
-
-const isProd = process.env.NODE_ENV === 'production';
+import { DB_NAME, DB_USER, DB_HOST, DB_PASSWORD } from '../utils/env';
 
 export const settings = {
-  database: isProd ? DB_PROD_NAME : DB_NAME,
+  database: DB_NAME,
   host: DB_HOST,
   username: DB_USER,
   password: DB_PASSWORD,
--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,19 +1,17 @@
 import cors, { CorsOptions } from 'cors';
+import { ORIGIN_CLIENT } from '../utils/env';
 
 const isProd = process.env.NODE_ENV === 'production';
-const whitelist: string[] = [
-	'https://gxanime.vercel.app',
-	'http://localhost:3000',
-];
+const whitelist: string[] = [ORIGIN_CLIENT as string];
 
 const options: CorsOptions = {
-	origin: (origin, cb) => {
-		if (isProd) {
-			!origin || whitelist.indexOf(origin) === -1
-				? cb(new Error('Not allowed by CORS'))
-				: cb(null, true);
-		} else cb(null, true);
-	},
+  origin: (origin, cb) => {
+    if (isProd) {
+      !origin || whitelist.indexOf(origin) === -1
+        ? cb(new Error('Not allowed by CORS'))
+        : cb(null, true);
+    } else cb(null, true);
+  },
 };
 
 const corsOrigin = () => cors(options);
