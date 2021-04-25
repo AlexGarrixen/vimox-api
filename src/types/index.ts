323aeb5e00a6450470af8f69310be68299ae08d0
alexTexis <alextexis.js@gmail.com>
2021-04-25 17:05:30 -0500

    update: remove preview image prop of episode model

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,17 +4,6 @@ export interface Route {
   path: string;
   handlers: RequestHandler[];
 }
-export interface Episode {
-  serie: string;
-  name: string;
-  sinopsis: string;
-  src: string;
-  thumbnail: string;
-  previewImage: string;
-  order: number;
-  createdAt: string;
-}
-
 export interface JwtPayload {
   userId: string;
   email: string;
